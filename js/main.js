6e57131f5eb86d06dd4cd39a2ce9b3d43bfde3d2
D. Czysz <derekczysz@gmail.com>
2019-03-02 16:53:24 -0600

    Initial upload

---
+++ js/main.js
@@ -0,0 +1,79 @@
+/* Current format
+
+  <div class="card" style="background-color: ${site.color};">
+    <div class="card-header">
+      <h3 class="card-title text-center">${site.name}</h3>
+    </div>
+    <div class="card-body">
+      <h5 class="card-title">Data</h5>
+      <p class="card-text">${site.data}</p>
+      <h5 class="card-title">Uses</h5>
+      <p class="card-text">${site.uses}</p>
+    </div>
+  </div>
+*/
+
+// Shuffle sites in random order
+fisherYatesShuffle(sites);
+
+for (let i = 0; i < sites.length; i++) {
+  let site = sites[i],
+      col = $('.card-columns')[0],
+      card = document.createElement('div'),
+      header = document.createElement('div'),
+      headerText = document.createElement('h3'),
+      body = document.createElement('div'),
+      dataTitle = document.createElement('h5'),
+      dataText = document.createElement('p'),
+      dataTextNode = document.createTextNode(site.data),
+      usesTitle = document.createElement('h5'),
+      usesText = document.createElement('p'),
+      usesTextNode = document.createTextNode(site.uses);
+
+  // Build card
+  card.classList.add('card');
+  // Add more info url if exists
+  if (site.info != '') {
+    card.style.cursor = 'pointer';
+    card.onclick = function() { window.location.href = site.info; }
+  }
+  card.style.background = site.color;
+
+    header.classList.add('card-header');
+      headerText.classList.add('card-title', 'text-center');
+      headerText.innerHTML = site.name;
+    header.appendChild(headerText);
+
+    body.classList.add('card-body');
+      dataTitle.classList.add('card-title');
+      dataTitle.innerHTML = 'Data';
+      dataText.classList.add('card-text');
+      dataText.appendChild(dataTextNode);
+
+      usesTitle.classList.add('card-title');
+      usesTitle.innerHTML = 'Uses';
+      usesText.classList.add('card-text');
+      usesText.appendChild(usesTextNode);
+    body.appendChild(dataTitle);
+    body.appendChild(dataText);
+    body.appendChild(usesTitle);
+    body.appendChild(usesText);
+
+  card.appendChild(header);
+  card.appendChild(body);
+  col.appendChild(card);
+}
+
+function fisherYatesShuffle(array) {
+  var i = array.length;
+  if (i == 0) return false;
+
+  while ( --i ) {
+     var j = Math.floor( Math.random() * ( i + 1 ) );
+     var tempi = array[i];
+     var tempj = array[j];
+     array[i] = tempj;
+     array[j] = tempi;
+   }
+   return array;
+}
---
+++ js/sites.js
@@ -0,0 +1,57 @@
+var sites = [
+  {
+    name: 'Facebook',
+    info: '',
+    color: '#4767aa',
+    data: 'All interactions on Facebook including quizzes and comments and statuses   you type but do not post, browsing info from pages with like buttons, location data, apps you and friends install, Instagram and WhatsApp data',
+    uses: 'Targeted advertising, facial recognition, Cambridge Analytica'
+   }, {
+    name: 'Google',
+    info: '',
+    color: '#4086f4',
+    data: 'Gmail content, drive files, calendar appointments, photos, searches, browsing history through page analytics, DoubleClick ads, and captchas, calls, texts, and location on Android',
+    uses: 'Targeted advertising'
+  }, {
+    name: 'Instagram',
+    info: '',
+    color: '#d0389f',
+    data: 'Uploaded content, contacts, location data',
+    uses: 'Targeted advertising, sharing with parent company Facebook, licensing of your photos with no reimbursement'
+  }, {
+    name: 'Microsoft',
+    info: '',
+    color: '#004185',
+    data: 'Device info, applications installed and usage details, crash info, browsing history, inferred interests, searches',
+    uses: 'Promotional communications, relevant offers, targeted advertising'
+  }, {
+    name: 'PayPal',
+    info: '',
+    color: '#203881',
+    data: 'Name, address, phone, email, contacts, financial account info, transaction info from each transaction including device info, usage and behavior data, and location',
+    uses: 'Location-based advertising, sharing with other advertising and analytics services and financial institutions'
+  }, {
+    name: 'Tinder',
+    info: '',
+    color: '#fe5665',
+    data: 'Facebook info like photos, locations, and statuses, device ID and type, Google and Apple advertising IDs, mobile and wireless network info like service provider, input from accelerometer, gyroscope, and compass, precise geolocation even while not using the app',
+    uses: 'Targeted advertising, full usage rights to your data including photos from linked accounts'
+  }, {
+    name: 'Uber',
+    info: '',
+    color: '#1e3944',
+    data: 'Name, credit card info, current location even when app is in background, regularly travelled locations',
+    uses: 'Marketing, data analytics and research, employees have full access to user data (which has been used to target reporters critical of Uber)'
+  }, {
+    name: 'Venmo',
+    info: '',
+    color: '#3d95ce',
+    data: 'Personal transaction info, contacts',
+    uses: 'Targeted advertising, sharing with parent company PayPal'
+  }, {
+    name: 'Verizon',
+    info: '',
+    color: '#cd040b',
+    data: 'Calls, texts, data, location, browsing history after logging into "My Verizon" on any device',
+    uses: 'Targeted advertising, selling location data'
+  }
+];
